59d16c2ea4a35a0eba65018f5006fb2bba08a36d
Andres Rengifo <andres.rengifo@bold.co>
2021-12-27 16:07:57 -0500

    feat: remove unused packages and fix dependencies

--- app/__init__.py
+++ app/__init__.py
@@ -1,36 +1,28 @@
 import os
+
 from flask import Flask, jsonify
 from flask_restful import Api
 
-from app.common.exceptions import ObjectNotFound, AppExceptionBaseClass
+from app.common.exceptions import AppExceptionBaseClass, ObjectNotFound
 from app.customers.api_v1_0.resources import customers_v1_0_bp
-from .ext import ma, migrate
+
+from .ext import migrate
 
 
 def create_app(settings_env):
     app = Flask(__name__)
     settings_module = f"config.default.{settings_env.capitalize()}Config"
     app.config.from_object(settings_module)
-    # Inicializa las extensiones
 
     from app.db import db
 
     db.init_app(app)
-    ma.init_app(app)
     migrate.init_app(app, db)
 
-    # Captura todos los errores 404
     Api(app, catch_all_404s=True)
-
-    # Deshabilita el modo estricto de acabado de una URL con /
     app.url_map.strict_slashes = False
-
-    # Registra los blueprints
     app.register_blueprint(customers_v1_0_bp)
-
-    # Registra manejadores de errores personalizados
     register_error_handlers(app)
-
     return app
 
 
