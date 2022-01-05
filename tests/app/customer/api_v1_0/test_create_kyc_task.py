4eb41339c533318563e48ed6febb4b67b9d290cd
Andres Rengifo <andres.rengifo@bold.co>
2022-01-05 13:05:20 -0500

    style: black formatting

--- tests/app/customer/api_v1_0/test_create_kyc_task.py
+++ tests/app/customer/api_v1_0/test_create_kyc_task.py
@@ -2,7 +2,8 @@ from mock import patch
 import pika
 from app.customers.api_v1_0 import tasks
 
+
 def test_create_a_kyc_task(app):
     with app.test_client() as test_client:
         with patch.object(pika, "BlockingConnection"):
-            tasks.create_kyc_task({"test":"test"})
+            tasks.create_kyc_task({"test": "test"})
